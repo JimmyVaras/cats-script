571d7440341b13cf2a688270be4d401043a50185
Jaime Varas Cáceres <35966995+JimmyVaras@users.noreply.github.com>
2023-03-06 00:11:21 +0100

    It works

--- main.py
+++ main.py
@@ -1,27 +1,5 @@
 import subprocess
 
-# Get user input for the CATS arguements
-contract = "--contract="+input("Enter the name of the .yml file of the contract (ex: openapi.yml): ")
-token = "Authorization="+input("Enter your access token to the API: ")
-server = "--server="+input("Enter the URL of the API (ex: https://openai.com/api): ")
-
-
-# Use subprocess to execute the command with arguments
-process = subprocess.Popen(['cats', contract, "-H", token, server, "-b -k"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-
-# cats --contract=openapi.yml -H "Authorization=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" --server=https://openai.com/api -b -k
-
-
-# Get output and error messages from subprocess
-output, error = process.communicate()
-
-# Print output and error messages
-print("Output: ", output.decode('utf-8'))
-print("Errors: ", error.decode('utf-8'))
-
-
-import subprocess
-
 # Prompt user for arguments
 contract = input("Enter contract file path: ")
 authorization = input("Enter authorization token: ")
@@ -30,7 +8,8 @@ b_flag = input("Include -b flag? (y/n): ").lower() == "y"
 k_flag = input("Include -k flag? (y/n): ").lower() == "y"
 
 # Construct command
-command = ["cats", "--contract={}".format(contract), "-H", "Authorization={}".format(authorization), "--server={}".format(server)]
+command = ["cats", "--contract={}".format(contract), "-H", "Authorization={}".format(
+    authorization), "--server={}".format(server)]
 if b_flag:
     command.append("-b")
 if k_flag:
@@ -38,3 +17,5 @@ if k_flag:
 
 # Execute command
 subprocess.run(command)
+
+# cats --contract=openapi.yml -H "Authorization=?????" --server=https://openai.com/api -b -k
