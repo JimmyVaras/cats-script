9330685cf8a33d3aa581e0fc3f6ff64cf7c741db
Jaime Varas Cáceres <35966995+JimmyVaras@users.noreply.github.com>
2023-03-05 23:53:23 +0100

    cats command

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 import subprocess
 
-# Get user input for the name of the file to find
-filename = input("Enter the name of the file to find: ")
+# Get user input for the CATS arguements
+contract = "--contract="+input("Enter the name of the .yml file of the contract (ex: openapi.yml): ")
+token = "Authorization="+input("Enter your access token to the API: ")
+server = "--server="+input("Enter the URL of the API (ex: https://openai.com/api): ")
 
-# Use subprocess to execute find command with arguments
-process = subprocess.Popen(['cats', '.', '-name', filename], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+# Use subprocess to execute the command with arguments
+process = subprocess.Popen(['cats', contract, "-H", token, server, "-b -k"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+# cats --contract=openapi.yml -H "Authorization=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" --server=https://openai.com/api -b -k
+
 
 # Get output and error messages from subprocess
 output, error = process.communicate()
