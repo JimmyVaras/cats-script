e0f2579693214119a0df1169c44444936a4a679a
Jaime Varas Cáceres <35966995+JimmyVaras@users.noreply.github.com>
2023-03-06 00:02:31 +0100

    added chatGPT code

--- main.py
+++ main.py
@@ -18,3 +18,23 @@ output, error = process.communicate()
 # Print output and error messages
 print("Output: ", output.decode('utf-8'))
 print("Errors: ", error.decode('utf-8'))
+
+
+import subprocess
+
+# Prompt user for arguments
+contract = input("Enter contract file path: ")
+authorization = input("Enter authorization token: ")
+server = input("Enter server URL: ")
+b_flag = input("Include -b flag? (y/n): ").lower() == "y"
+k_flag = input("Include -k flag? (y/n): ").lower() == "y"
+
+# Construct command
+command = ["cats", "--contract={}".format(contract), "-H", "Authorization={}".format(authorization), "--server={}".format(server)]
+if b_flag:
+    command.append("-b")
+if k_flag:
+    command.append("-k")
+
+# Execute command
+subprocess.run(command)
